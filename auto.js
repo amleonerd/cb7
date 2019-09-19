fb735b5b94c17fc293e515e0fed4a923d24a1d38
amleonerd <55100142+amleonerd@users.noreply.github.com>
2019-09-19 13:41:10 -0400

    Add files via upload

---
+++ auto.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    var pets = document.forms["cats" , "dogs", "penguins"].value;
+   // if (x == "") 
+    //{
+     // alert("Pet");
+      //return false;
+    //}
+    //compare
+  myObj = {name: "cats", "dogs", "penguins"};
+  myJSON = JSON.stri(myObngifyj);
+  localStorage.setItem("testJSON", myJSON);
+  }
+  

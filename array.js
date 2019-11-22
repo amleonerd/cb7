cdb1c0aa701dc010d12fc5b8d5b6e5fcd8a49d5f
amleonerd <55100142+amleonerd@users.noreply.github.com>
2019-11-22 11:48:31 -0500

    Array HW

--- array.js
+++ array.js
@@ -1,32 +1,9 @@
-//i want you all to make an array and use map, filter, reduce to do something cool
-//then
-//make a mocha test for it! :/
-let convert = {};
-var class = 
-var theGoodPlace = [
-  { id: 32, name: "Eleanor", home: "Good Place"},
-  { id: 34, name: "Cheety", home: "Good Place" },
-  { id: 28, name: "Tahani Al-Jamil" , home: "Bad Place"},
-  { id: 100, name: "The Good Janet", home: "Bad Place" }
-];
-//reduce function to find combined age/average age 
-var combinedAge = theGoodPlace.reduce(function (accumulator, pilot) {
-    var subjects = 4; 
-    var totalAge = accumulator + theGoodPlace.id;
-    return totalAge;
-  }, 0);
-  console.log(combinedAge/4);
-  //map function 
-  var theGoodPlaceCharecters = theGoodPlace.map(function(tgp) {
-    return theGoodPlace.id
-});
-  
-  //filter function to find home 
+function frankenSplice(arr1, arr2, n) {
+    var arr1Copy = arr1.slice();
+    var arr2Copy = arr2.slice();
 
-  var badPlace = theGoodPlace.filter(function (tgp) {
-    return tgp.home === "Bad Place";
-  });
-  var goodPlace = theGoodPlace.filter(function (tgp) {
-    return tgp.home === "Good Place";
-  });
-  module.exports = convert;
+    for (var i = 0; i < arr1.length; i++) {
+        arr2Copy.splice(n+i, 0, arr1Copy[i]);
+    }
+    return arr2Copy;
+}
